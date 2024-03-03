7da1508bea65d239035d43bf271ef4981cf8fc1a
arturs706 <aradionovs@yahoo.com>
2024-03-03 23:37:19 +0000

    first-commit

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,14 +1,7 @@
-import { Component, createSignal, createEffect } from "solid-js";
+import { Component } from "solid-js";
 import Mainpage from "../components/pages/Mainpage";
 
 const Home: Component = () => {
-  const [showMainSection, setShowMainSection] = createSignal(true);
-
-  createEffect(() => {
-    setTimeout(() => {
-      setShowMainSection(false);
-    }, 1900); // Set to 1 second (1000 milliseconds)
-  });
 
   return (
     <>
